7b49edca2031fc2eea910dd66824446dacc90628
Konstantin <konstantin.yakovlev@grphn.ru>
2024-08-26 15:36:09 +0400

    fix empty edit, data in html

--- app.js
+++ app.js
@@ -1,55 +1,57 @@
 const App = {
-    data() {
-        return {
-            title: "Notes",
-            input: {
-                value: "",
-                placeholder: "Type your note",
-            },
-            editInput: null,
-            button: {
-                text: "Add note",
-            },
-            notes: ["Task 1", "Task 2", "Task 3"],
-            inEditItem: false,
-            editText: "",
-        };
+  data() {
+    return {
+      input: {
+        value: "",
+      },
+      editInput: {
+        editInput: null,
+        editText: "",
+        inEditItem: false,
+      },
+      notes: ["Task 1", "Task 2", "Task 3"],
+    };
+  },
+  mounted() {
+    this.getNotes();
+  },
+  watch: {
+    notes: {
+      handler(updatedList) {
+        localStorage.setItem("items", JSON.stringify(updatedList));
+      },
+      deep: true,
     },
-    mounted() {
-        this.getNotes();
+  },
+  methods: {
+    getNotes() {
+      const localNotes = localStorage.getItem("items");
+      if (localNotes) {
+        this.notes = JSON.parse(localNotes);
+      }
     },
-    watch: {
-        notes: {
-            handler(updatedList) {
-                localStorage.setItem("items", JSON.stringify(updatedList));
-            },
-            deep: true,
-        },
+    onSubmit() {
+      this.notes.push(this.input.value);
+      this.input.value = "";
     },
-    methods: {
-        getNotes() {
-            const localNotes = localStorage.getItem("items");
-            if (localNotes) {
-                this.notes = JSON.parse(localNotes);
-            }
-        },
-        onSubmit() {
-            this.notes.push(this.input.value);
-            this.input.value = "";
-        },
-        remove(index) {
-            this.notes.splice(index, 1);
-        },
-        edit(item) {
-            this.editText = item;
-            this.editInput = item;
-            this.inEditItem = true;
-        },
-        saved(index) {
-            this.notes[index] = this.editText;
-            this.inEditItem = false;
-        },
+    remove(index) {
+      this.notes.splice(index, 1);
     },
+    edit(item) {
+      this.editInput.editText = item;
+      this.editInput.editInput = item;
+      this.editInput.inEditItem = true;
+    },
+    saved(index) {
+      if (this.editInput.editText !== "") {
+        this.notes[index] = this.editInput.editText;
+      }
+
+      this.editInput.editText = "";
+      this.editInput.editInput = null;
+      this.editInput.inEditItem = false;
+    },
+  },
 };
 
 Vue.createApp(App).mount("#app");
